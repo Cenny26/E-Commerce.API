eb9548d84154f8594cc4c218a1eb4db04e5234fb
Kanan Huseynov <info.kennans26@gmail.com>
2024-03-22 14:13:39 +0400

    Creation of entities required for products and products has been completed.

---
+++ src/Core/ECommerce.Domain/Entities/Brand.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Domain.Common;
+
+namespace ECommerce.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public Brand()
+        {
+            
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+
+        public required string Name { get; set; }
+    }
+}
