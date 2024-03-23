9aaef985fe554fb5efedddbfc44a691bdeab97a9
Kanan Huseynov <info.kennans26@gmail.com>
2024-03-23 21:31:21 +0400

    Entity configurations and fake data seed operations with Bogus have been completed.

---
+++ src/Infrastructure/ECommerce.Persistence/Configurations/BrandConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Bogus;
+using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.Persistence.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            Faker faker = new Faker(locale: "en");
+
+            builder.HasData(new Brand()
+            {
+                Id = 1,
+                Name = faker.Commerce.Department(),
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            },
+            new Brand()
+            {
+                Id = 2,
+                Name = faker.Commerce.Department(),
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            },
+            new Brand()
+            {
+                Id = 3,
+                Name = faker.Commerce.Department(),
+                CreatedDate = DateTime.Now,
+                IsDeleted = true
+            });
+        }
+    }
+}
---
+++ src/Infrastructure/ECommerce.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(new Category()
+            {
+                Id = 1,
+                ParentId = 0,
+                Name = "Electronic",
+                Priority = 1,
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            },
+            new Category()
+            {
+                Id = 2,
+                ParentId = 0,
+                Name = "Fashion",
+                Priority = 2,
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            },
+            new Category()
+            {
+                Id = 3,
+                ParentId = 1,
+                Name = "Computer",
+                Priority = 1,
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            },
+            new Category()
+            {
+                Id = 4,
+                ParentId = 2,
+                Name = "Woman",
+                Priority = 1,
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            });
+        }
+    }
+}
