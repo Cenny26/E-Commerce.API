55876271f5c58e35a034d707e6e118dac21a8aad
Kanan Huseynov <info.kennans26@gmail.com>
2024-03-24 20:55:49 +0400

    Dynamically, the Unit Of Work structure was established.

---
+++ src/Infrastructure/ECommerce.Persistence/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Application.Interfaces.Repositories;
+using ECommerce.Application.Interfaces.UnitOfWorks;
+using ECommerce.Persistence.Context;
+using ECommerce.Persistence.Repositories;
+
+namespace ECommerce.Persistence.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _dbContext;
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async ValueTask DisposeAsync() => await _dbContext.DisposeAsync();
+
+        public int Save() => _dbContext.SaveChanges();
+
+        public async Task<int> SaveAsync() => await _dbContext.SaveChangesAsync();
+
+        IReadRepository<T> IUnitOfWork.GetReadRepository<T>() => new ReadRepository<T>(_dbContext);
+
+        IWriteRepository<T> IUnitOfWork.GetWriteRepository<T>() => new WriteRepository<T>(_dbContext);
+    }
+}
